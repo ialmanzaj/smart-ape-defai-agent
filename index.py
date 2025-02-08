df3acf11f8c2483febf08a28880149300b2e8802
Isaac Almanza <isaac.almanza19@gmail.com>
2025-02-08 12:53:45 -0500

    feat: remove nft

--- index.py
+++ index.py
@@ -6,7 +6,7 @@ from agent.initialize_agent import initialize_agent
 from agent.run_agent import run_agent
 from db.setup import setup
 from db.tokens import get_tokens
-from db.nfts import get_nfts
+
 
 load_dotenv()
 app = Flask(__name__)
@@ -53,14 +53,7 @@ def tokens():
         return jsonify({'error': 'An unexpected error occurred'}), 500
     
 # Retrieve a list of tokens the agent has deployed
-@app.route("/nfts", methods=['GET'])
-def nfts():
-    try:
-        nfts = get_nfts()
-        return jsonify({'nfts': nfts}), 200
-    except Exception as e:
-        app.logger.error(f"Unexpected error in nfts endpoint: {str(e)}")
-        return jsonify({'error': 'An unexpected error occurred'}), 500
+
 
 if __name__ == "__main__":
     app.run()
